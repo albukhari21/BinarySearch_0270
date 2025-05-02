d3f9a05a83e541260b830fbfd8eef0b820be9ba7
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-02 09:13:29 +0700

    memasukkan element array

--- file1.cpp
+++ file1.cpp
@@ -5,14 +5,105 @@ int element[10];
 int nPanjang;
 int x;
 
-void binarySearch() {
-    cout << "Masukkan element yang ingin dicari = ";
-    cin >> x; //step 1
-    int low = 0;  // step 2
-    int high = nPanjang-1; // step 3
-    int mid = (low + high)/2; // step 4
+void input()
+{ // Prosedur input
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array: ";
+        cin >> nPanjang;
+        if (nPanjang <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nArray maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukan elemen array" << endl;
+    cout << "====================" << endl;
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
 }
 
-int main(){
+void bubbleSortArray()
+{
+    int pass = 1; // step 1
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        { // step 2
+            if (element[j] > element[j + 1])
+            { // step 3
+                int temp;
+                temp = element[j];
+                element[j] = element[j] + 1;
+                element[j + 1] = temp;
+            }
+        }
+        pass = pass + 1; // step 4
+    } while (pass <= nPanjang - 1); // step 5
+}
 
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " --> ";
+        }
+    }
+}
+
+void binarySearch()
+{
+    cout << "Masukkan element yang ingin dicari = ";
+    cin >> x;                // step 1
+    int low = 0;             // step 2
+    int high = nPanjang - 1; // step 3
+    do
+    {
+        int mid = (low + high) / 2; // step 4
+        if (element[mid] == x)
+        {                                // step 5
+            cout << "Ditemukan pada index" << endl; // step 5
+            return;
+        }
+        if (x < element[mid])
+        {                   // step 6
+            high = mid - 1; // step 6
+        }
+        if (x > element[mid])
+        {                  // step 7
+            low = mid + 1; // step 7a
+        }
+        if (low <= high)
+        {
+        }
+    } while (low <= high); // step 8
+    if (low > high)
+    {
+        cout << x << " Tidak ditemukan " << endl; // step 9
+    }
+}
+
+int main()
+{
+    input();
+    bubbleSortArray();
+    display();
+    binarySearch();
 }
