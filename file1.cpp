449f019c87ee18e79ab9404f8b4268d538547c7a
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-02 08:01:44 +0700

    menambahkan item-item

---
+++ file1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void binarySearch() {
+    cout << "Masukkan element yang ingin dicari = ";
+    cin >> x; //step 1
+    int low = 0;  // step 2
+    int high = nPanjang-1; // step 3
+    int mid = (low + high)/2; // step 4
+}
+
+int main(){
+
+}
